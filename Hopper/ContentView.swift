75af05024abaf146f303923137c603b4a6adcb6f
Grant Starkman <grant@thatch.tech>
2024-02-05 13:54:28 -0700

    Initial Commit

---
+++ Hopper/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Hopper
+//
+//  Created by Grant Starkman on 2/5/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Hopper/HopperApp.swift
@@ -0,0 +1,17 @@
+//
+//  HopperApp.swift
+//  Hopper
+//
+//  Created by Grant Starkman on 2/5/24.
+//
+
+import SwiftUI
+
+@main
+struct HopperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
